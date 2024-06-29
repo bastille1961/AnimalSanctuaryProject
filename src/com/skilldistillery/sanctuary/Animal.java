2906ea4517937d762e7a7a001d062d1395d2cf56
johnroberts <bastille1961@gmail.com>
2024-06-28 20:03:27 -0400

    Create getName, makeNoise, int amountFood

--- src/com/skilldistillery/sanctuary/Animal.java
+++ src/com/skilldistillery/sanctuary/Animal.java
@@ -1,10 +1,24 @@
 package com.skilldistillery.sanctuary;
 
 public class Animal {
-
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void makeNoise() {
+		System.out.println("Animal making noise ...");
+	}
+	
+	public void eat(int amountOfFood) {
+		System.out.println("I'm eating " + amountOfFood + " foods.");
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Animal animal = new Animal();
+		animal.makeNoise();
+		animal.eat(9);	
 	}
 
 }
